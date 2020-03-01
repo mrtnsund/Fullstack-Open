31b56c99113dee2526318a40cbdbc9f2d095ae18
Morten Sund <mrtnsund@gmail.com>
2020-03-01 17:46:59 +0100

    forsøk på velgeknapp

--- part2/dataforcountries/src/components/DisplayCountries.js
+++ part2/dataforcountries/src/components/DisplayCountries.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import DisplaySpecificCountry from './DisplaySpecificCountry'
 
 const DisplayCountries = ( {countries} ) => 
     countries.map((country, i) => {
     return (
+    <div>
       <li key={i}>{country.name}</li>
+    </div>
     )
   })
 
