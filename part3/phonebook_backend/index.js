07453ff801765d90842d4baeda709bba486f9db6
Morten Sund <mrtnsund@gmail.com>
2020-03-05 07:56:04 +0100

    endret get-metoden til å hente data fra mongodb

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+const Person = require('./models/person')
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -14,32 +16,25 @@ morgan.token('body', function (req, res){
 app.use(morgan(':method :url :status :response-time ms :res[content-length] :body :req[content-length]'))
 
 
-
-let persons = [
-    {
-        name: "Morten Sund",
-        number: 41596565,
-        id: 1
-    },
-    {
-        name: "Hanna Brodersen",
-        number: 95261243,
-        id: 2
-    }
-]
-
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(persons => {
+        res.json(persons.map(person => person.toJSON()))
+    })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person){
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
+    Person.findById(req.params.id)
+    .then(person => {
+        if(person){
+            res.json(person.toJSON())
+        } else {
+            res.status(404).end()
+        }
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(404).send({error: 'malformatted id'})
+    })
 })
 app.post('/api/persons', (req, res) => {
     const body = req.body
@@ -47,29 +42,29 @@ app.post('/api/persons', (req, res) => {
         return res.status(400).json({
             error: 'name or number missing'
         })
-    } else if (persons.find(person => (person.name === body.name || person.number === body.number))){
-        return res.status(400).json({
-            error: 'name or number already in use'
-        })
+    // } else if (persons.find(person => (person.name === body.name || person.number === body.number))){
+    //     return res.status(400).json({
+    //         error: 'name or number already in use'
+    //     })
     } else {
-        const person = {
+        const person = new Person({
             name: body.name,
             number: body.number,
             id: Math.floor(Math.random()*999999)
-        }
-        persons=persons.concat(person)
-        res.json(person)
+        })
+        person.save().then(savedPerson => {
+            res.json(savedPerson.toJSON())
+        })
     }
 })
+
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-    res.status(204).end()
+    Person.findById(req.params.id).then(person => {
+        res.json(person.toJSON())
+    })
 })
-app.get('/info', (req, res) => {
-    res.send(`<p>Phonebook has info for ${persons.length} people<p><br/>${Date()}`)
-})
-const PORT = process.env.PORT || 3001
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
 })
