8c77f22c9dbf28d3ebe30370fe6bafd29501d1b7
Morten <mrtnsund@gmail.com>
2020-03-06 22:30:50 +0100

    fikset frontend-display av feilmld

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -64,7 +64,7 @@ const App = () => {
           }, 3000)
         })
         .catch(error => {
-          setNotification(`${error}`)
+          setNotification(`${error.response.data.error}`)
           setTimeout(() => {
             setNotification(null)
           }, 3000)
