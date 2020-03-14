2a289c24db58600885813c9e27e48480df68b7b0
Morten <mrtnsund@gmail.com>
2020-03-14 18:15:29 +0100

    la til select på displaycountries

--- part2/dataforcountries/src/components/DisplayCountries.js
+++ part2/dataforcountries/src/components/DisplayCountries.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
+import DisplaySpecificCountry from './DisplaySpecificCountry'
+import '../css/output.css'
 
 
-const DisplayCountries = ( {countries} ) => 
-    countries.map((country, i) => <li className="py-1" key={i}>{country.name}</li>)
-
-
+const DisplayCountries = ( {countries} ) => {
+  const [index, setIndex] = useState(null)
+  if (index !== null){
+    return (
+      <DisplaySpecificCountry country={countries[index]} />
+    )
+  } else {
+    return (
+    countries.map((country, i) => 
+    <li className="py-2" key={i}>{country.name}
+      <button key={country.name}className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-1 border border-gray-400 rounded shadow text-xs" onClick={() => setIndex(i)}>
+        show
+      </button>
+    </li>)
+    )
+  }
+}
 export default DisplayCountries
